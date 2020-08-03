ab3520294a89798e563aed6530e2f6b3b3c48fe6
Luis Santos <luisfcs@hotmail.co.uk>
2020-08-03 21:17:29 +0100

    read headers to apply auth and limit api access

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-ProxyShed
+# ProxyShed
+
+
+Example environment variables for API Server:
+
+env.file
+
+```sh
+export JWT_SECRET=1234
+export DEBUG=True
+```
--- Server/app.py
+++ Server/app.py
@@ -1,18 +1,39 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from os import environ
 import jwt
 
 
 app = Flask(__name__)
 api = Api(app, prefix="/api/v1")
 
+app.config['SECRET_KEY'] = environ['JWT_SECRET']
+
+def auth_required(f):
+    def decorated(*args):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return {'message':'Token is missing'}, 401
+
+        try:
+            ## VALIDATION NEEDED!!! login and validate token.
+            return {'message':'youre all good'}, 200
+        except:
+            return {'message':'Invalid Token'}, 401
+        return f( *args)
+    
+    return decorated
 
 class root(Resource):
-
+    @auth_required
     def get(self):
         return {"test":"object"}
 
 api.add_resource(root, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=environ['DEBUG'])
