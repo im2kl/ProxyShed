9956cdd7bbfce5bd0382bc0d8da5dc0f51f3b327
Luis Santos <luisfcs@hotmail.co.uk>
2020-08-12 22:12:00 +0100

    soted our initialization of config.

--- Client/config/config.go
+++ Client/config/config.go
@@ -1,7 +1,20 @@
 package config
 
-// AdminDash is the api url for admin information
-type AdminDash struct {
-	url   string
+// adminDash is the api url for admin information
+type adminDash struct {
+	// URL admin url for dashboard
+	url string
+	// Token Connection token / license
 	token string
+}
+
+// Init initialize the config
+func Init() {
+
+	conf := adminDash{}
+
+	//get from secure source or hardcode?
+	conf.token = "asd"
+	conf.url = "proxysource.pnxbl.com"
+
 }
